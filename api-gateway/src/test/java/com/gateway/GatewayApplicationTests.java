3b05d23cbb65045103d9cd4fba9f7443397e1e3e
Jake Lemar <jalema01@wsc.edu>
2020-03-30 21:41:19 -0500

    changed name of java app

---
+++ api-gateway/src/test/java/com/gateway/GatewayApplicationTests.java
@@ -0,0 +1,14 @@
+ 
+package com.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
