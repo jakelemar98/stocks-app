fff4864b10e39780ac5fb84856056e4018c78f71
Jake Lemar <jalema01@wsc.edu>
2020-04-03 01:36:07 -0500

    test workflow

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -3,14 +3,16 @@
 set -e
 
 GITHUB_SHA=$1
+DEPLOYMENT=$2
 
+echo $2
 # We must export it so it's available for envsubst
 export GITHUB_SHA=$1
 
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
-envsubst <./k8s/gateway-deployment.yml >./k8s/gateway-deployment.yml.out
-mv ./k8s/gateway-deployment.yml.out ./k8s/gateway-deployment.yml
+envsubst <./k8s/$2-deployment.yml >./k8s/$2-deployment.yml.out
+mv ./k8s/$2-deployment.yml.out ./k8s/$2-deployment.yml
 
-./kubectl apply -f ./k8s/gateway.yml
+./kubectl apply -f ./k8s/$2-deployment.yml
