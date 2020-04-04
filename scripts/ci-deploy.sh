eeae1f62984783fb9f2217912a3bb0baec520e33
Jack Temple <jetemple@iastate.edu>
2020-04-03 23:03:34 -0500

    ci-deploy v 2.01

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -11,7 +11,7 @@ export GITHUB_SHA=$1
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
-envsubst <./k8s/$2-deployment.yml >./k8s/$2/$2-deployment.yml.out
-mv ./k8s/$2-deployment.yml.out ./k8s/$2/$2-deployment.yml
+envsubst <./k8s/$2/$2-deployment.yml >./k8s/$2/$2-deployment.yml.out
+mv ./k8s/$2/$2-deployment.yml.out ./k8s/$2/$2-deployment.yml
 
 kubectl apply -f ./k8s/$2/$2-deployment.yml
