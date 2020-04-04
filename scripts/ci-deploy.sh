7f459a48b45c3cc0d183fa8d84888eda074582b8
Jake Lemar <jalema01@wsc.edu>
2020-04-04 17:20:33 -0500

    fixing shit

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -7,7 +7,7 @@ DEPLOYMENT=$2
 
 # We must export it so it's available for envsubst
 export GITHUB_SHA=$1
-
+export BRANCH=$7
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
