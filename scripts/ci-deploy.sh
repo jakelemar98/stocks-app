caf87ffccbe68f3ea5c797a5fca3e59bb6c3b30c
Jake Lemar <jalema01@wsc.edu>
2020-04-04 02:36:47 -0500

    trying new deployment with helm

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -11,8 +11,8 @@ export GITHUB_SHA=$1
 # since the only way for envsubst to work on files is using input/output redirection,
 #  it's not possible to do in-place substitution, so we need to save the output to another file
 #  and overwrite the original with that one.
-envsubst <./k8s/$2/values.yml >./k8s/$2/values.yml.out
-mv ./k8s/$2/values.yml.out ./k8s/$2/values.yml
+envsubst <./k8s/$2/values.yaml >./k8s/$2/values.yaml.out
+mv ./k8s/$2/values.yaml.out ./k8s/$2/values.yaml
 
 kubectl config use-context $3
 
