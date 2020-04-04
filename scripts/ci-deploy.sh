309d65e96a375ef37c8764a84cbcb9c8cad0b67f
Jake Lemar <jalema01@wsc.edu>
2020-04-04 03:16:02 -0500

    trying new deployment with helm

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -25,4 +25,4 @@ curl -fsSL -o get_helm.sh https://raw.githubusercontent.com/helm/helm/master/scr
 chmod 700 get_helm.sh
 ./get_helm.sh
 
-helm install $2 ./k8s/$2
+helm upgrade --install $2 ./k8s/$2
