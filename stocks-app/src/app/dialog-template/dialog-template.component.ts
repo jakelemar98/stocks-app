f53677269960b800d0386f933de3dd709cbc1ca0
Jake Lemar <jalema01@wsc.edu>
2020-04-09 03:26:49 -0500

    added a lot of angular stuff

---
+++ stocks-app/src/app/dialog-template/dialog-template.component.ts
@@ -0,0 +1,24 @@
+import { Component, Inject } from '@angular/core';
+import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+
+export interface DialogData {
+  issue: string;
+}
+
+
+@Component({
+  selector: 'dialog-template',
+  templateUrl: 'dialog-template.component.html',
+})
+export class DialogTemplateComponent {
+
+  constructor(
+    public dialogRef: MatDialogRef<DialogTemplateComponent>,
+    @Inject(MAT_DIALOG_DATA) public data: DialogData) {}
+
+  onNoClick(): void {
+    this.dialogRef.close();
+  }
+
+}
--- stocks-app/src/app/register/register.component.ts
+++ stocks-app/src/app/register/register.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
 import { FormBuilder } from '@angular/forms';
 import { Validators } from '@angular/forms';
 import { EmailValidator } from './email.validator';
@@ -6,6 +6,8 @@ import { PasswordValidator } from './password.validator';
 import { UsersService } from '../users.service';
 import { NewUser } from '../new-user'
 import { User } from '../user';
+import { DialogTemplateComponent } from '../dialog-template/dialog-template.component'
+import { MatDialogRef, MatDialog, MAT_DIALOG_DATA } from '@angular/material/dialog';
 
 @Component({
   selector: 'app-register',
@@ -16,18 +18,26 @@ export class RegisterComponent implements OnInit {
   
   formData: Object;
   user: User;
+  response: Object;
+  error: any;
+
+  dialogTemplate: MatDialogRef<DialogTemplateComponent>
+
   registerForm = this.fb.group({
     firstname: ['', Validators.required],
     lastname: ['', Validators.required],
-    email: ['', Validators.required],
-    confirmEmail: ['', Validators.required],
+    email: ['', [Validators.required, Validators.email]],
+    confirmEmail: ['', [Validators.required, Validators.email]],
     password: ['', [Validators.required, Validators.minLength(8)]],
     confirmPass: ['', Validators.required]
   }, { validator: [EmailValidator.mustBeEqual, PasswordValidator.mustBeEqual] } )
 
-  constructor(private fb: FormBuilder, private userHttp: UsersService) { }
+  
+
+  constructor(private fb: FormBuilder, private userHttp: UsersService, private dialog: MatDialog) { }
 
   ngOnInit(): void {
+
   }
 
   onRegister() {
@@ -39,10 +49,33 @@ export class RegisterComponent implements OnInit {
       password: this.formData['password']
     }
 
-    this.userHttp.createUser(user).subscribe(data => {
-      console.log(data);
-    })
+    this.userHttp.createUser(user).subscribe(
+      data => {
+        this.response = data
+        this.dialogTemplate = this.dialog.open(DialogTemplateComponent, {
+          width: "250px",
+          data: {message: "Your account has been created! lets get started!", title: "Nice!", showButton: true}
+        });
+      },
+      error =>  {
+        this.error = error
 
+        if(this.error['status'] !== 201) {          
+          this.dialogTemplate = this.dialog.open(DialogTemplateComponent, {
+            width: "250px",
+            data: {message: this.error, title: "Whoops"}
+          });
+        }
+      }
+
+      
+    )    
+  }
+
+  private success(data){
+    this.response = data
+      console.log(this.response);
   }
 
 }
+
